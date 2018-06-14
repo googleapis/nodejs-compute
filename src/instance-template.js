d1314944cb9a5079faafa761a73d9e4ddf354bcd
Romain Beauxis <toots@rastageeks.org>
2018-06-14 22:29:49 +0200

    Getting started with instanceTemplate.

---
+++ src/instance-template.js
@@ -0,0 +1,286 @@
+/*!
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var common = require('@google-cloud/common');
+var extend = require('extend');
+var is = require('is');
+var util = require('util');
+
+var Operation = require('./operation.js');
+
+/**
+ * @class
+ * @param {Compute} compute - Compute object this instance template belongs to.
+ * @param {string} name
+ *
+ * @example
+ * const Compute = require('@google-cloud/compute');
+ * const compute = new Compute();
+ * const instanceTemplate = compute.instanceTemplate('my-instance-template');
+ */
+function InstanceTemplate(compute, name) {
+  var methods = {
+    /**
+     * Check if the instance template exists.
+     *
+     * @method InstanceTemplate#exists
+     * @param {function} callback - The callback function.
+     * @param {?error} callback.err - An error returned while making this
+     *     request.
+     * @param {boolean} callback.exists - Whether the instance group exists or
+     *     not.
+     *
+     * @example
+     * const Compute = require('@google-cloud/compute');
+     * const compute = new Compute();
+     * const instanceTemplate = compute.instanceTemplate('my-instance-template');
+     *
+     * instanceTemplate.exists(function(err, exists) {});
+     *
+     * //-
+     * // If the callback is omitted, we'll return a Promise.
+     * //-
+     * instanceTemplate.exists().then(function(data) {
+     *   const exists = data[0];
+     * });
+     */
+    exists: true,
+
+    /**
+     * Get an instance template if it exists.
+     *
+     * You may optionally use this to "get or create" an object by providing an
+     * object with `autoCreate` set to `true`. Any extra configuration that is
+     * normally required for the `create` method must be contained within this
+     * object as well.
+     *
+     * @method InstanceTemplate#get
+     * @param {options=} options - Configuration object.
+     * @param {boolean} options.autoCreate - Automatically create the object if
+     *     it does not exist. Default: `false`
+     *
+     * @example
+     * const Compute = require('@google-cloud/compute');
+     * const compute = new Compute();
+     * const instanceTemplate = compute.instanceTemplate('my-instance-template');
+     *
+     * instanceTemplate.get(function(err, instanceTemplate, apiResponse) {
+     *   // `instanceTemplate` is an InstanceTemplate object.
+     * });
+     *
+     * //-
+     * // If the callback is omitted, we'll return a Promise.
+     * //-
+     * instanceTemplate.get().then(function(data) {
+     *   const instanceTemplate = data[0];
+     *   const apiResponse = data[1];
+     * });
+     */
+    get: true,
+
+    /**
+     * Get the instance template's metadata.
+     *
+     * @see [InstanceTemplates: get API Documentation]{@link https://cloud.google.com/compute/docs/reference/v1/instanceTemplates/get}
+     * @see [InstanceTemplates Resource]{@link https://cloud.google.com/compute/docs/reference/v1/instanceTemplates}
+     *
+     * @method InstanceTemplate#getMetadata
+     * @param {function=} callback - The callback function.
+     * @param {?error} callback.err - An error returned while making this
+     *     request.
+     * @param {object} callback.metadata - The instance group's metadata.
+     * @param {object} callback.apiResponse - The full API response.
+     *
+     * @example
+     * const Compute = require('@google-cloud/compute');
+     * const compute = new Compute();
+     * const instanceTemplate = compute.instanceTemplate('my-instance-template');
+     *
+     * instanceTemplate.getMetadata(function(err, metadata, apiResponse) {});
+     *
+     * //-
+     * // If the callback is omitted, we'll return a Promise.
+     * //-
+     * instanceTemplate.getMetadata().then(function(data) {
+     *   const metadata = data[0];
+     *   const apiResponse = data[1];
+     * });
+     */
+    getMetadata: true,
+  };
+
+  common.ServiceObject.call(this, {
+    parent: compute,
+    baseUrl: '/global/instanceTemplates',
+    /**
+     * @name InstanceTemplate#id
+     * @type {string}
+     */
+    id: name,
+    methods: methods,
+  });
+
+  /**
+   * The parent {@link Compute} instance of this {@link InstanceTemplate} instance.
+   * @name InstanceTemplate#compute
+   * @type {Compute}
+   */
+  this.compute = compute;
+
+  /**
+   * @name InstanceTemplate#name
+   * @type {string}
+   */
+  this.name = name;
+}
+
+util.inherits(InstanceTemplate, common.ServiceObject);
+
+/**
+ * Create an instance template.
+ *
+ * @method InstanceTemplate#create
+ * @param {object=} options - See an
+ *     [InstanceTemplate resource](https://cloud.google.com/compute/docs/reference/v1/instanceTemplate#resource).
+ *
+ * @example
+ * const Compute = require('@google-cloud/compute');
+ * const compute = new Compute();
+ * const instanceTemplate = compute.instanceTemplate('my-instance-template');
+ *
+ * function onCreated(err, instanceTemplate, operation, apiResponse) {
+ *   // `instanceTemplate` is an InstanceTemplate object.
+ *
+ *   // `operation` is an Operation object that can be used to check the
+ *   // status of the request.
+ * }
+ *
+ * instanceTemplate.create(options, onCreated);
+ *
+ * //-
+ * // If the callback is omitted, we'll return a Promise.
+ * //-
+ * instanceTemplate.create(options).then(function(data) {
+ *   const instanceTemplate = data[0];
+ *   const operation = data[1];
+ *   const apiResponse = data[2];
+ * });
+ */
+InstanceTemplate.prototype.create = function(options, callback) {
+  var self = this;
+
+  callback = callback || common.util.noop;
+
+  var body = extend({}, options, {
+    name: self.name,
+  });
+
+  this.request(
+    {
+      method: 'POST',
+      uri: '',
+      json: body,
+    },
+    function(err, resp) {
+      if (err) {
+        callback(err, null, null, resp);
+        return;
+      }
+
+      var operation = self.operation(resp.name);
+      operation.metadata = resp;
+
+      callback(null, self, operation, resp);
+    }
+  );
+};
+
+/**
+ * Delete the instance template.
+ *
+ * @see [InstanceTemplates: delete API Documentation]{@link https://cloud.google.com/compute/docs/reference/v1/instanceTemplates/delete}
+ *
+ * @param {function=} callback - The callback function.
+ * @param {?error} callback.err - An error returned while making this request.
+ * @param {Operation} callback.operation - An operation object
+ *     that can be used to check the status of the request.
+ * @param {object} callback.apiResponse - The full API response.
+ *
+ * @example
+ * const Compute = require('@google-cloud/compute');
+ * const compute = new Compute();
+ * const instanceTemplate = compute.instanceTemplate('my-instance-template');
+ *
+ * instanceTemplate.delete(function(err, operation, apiResponse) {
+ *   // `operation` is an Operation object that can be used to check the status
+ *   // of the request.
+ * });
+ *
+ * //-
+ * // If the callback is omitted, we'll return a Promise.
+ * //-
+ * instanceTemplate.delete().then(function(data) {
+ *   const operation = data[0];
+ *   const apiResponse = data[1];
+ * });
+ */
+InstanceTemplate.prototype.delete = function(callback) {
+  var self = this;
+
+  callback = callback || common.util.noop;
+
+  common.ServiceObject.prototype.delete.call(this, function(err, resp) {
+    if (err) {
+      callback(err, null, resp);
+      return;
+    }
+
+    var operation = self.operation(resp.name);
+    operation.metadata = resp;
+
+    callback(null, operation, resp);
+  });
+};
+
+/**
+ * Get a reference to a Google Compute Engine global operation.
+ *
+ * @see [Global Operation Overview]{@link https://cloud.google.com/compute/docs/reference/v1/globalOperations}
+ *
+ * @param {string} name - Name of the existing operation.
+ * @returns {Operation}
+ *
+ * @example
+ * const Compute = require('@google-cloud/compute');
+ * const compute = new Compute();
+ * const instanceTemplate = compute.instanceTemplate('my-instance-template');
+ *
+ * const operation = instanceTemplate.operation('operation-1445532685163-8b137d2a-1822afe7');
+ */
+InstanceTemplate.prototype.operation = function(name) {
+  return new Operation(this.compute, name);
+};
+
+/*! Developer Documentation
+ *
+ * All async methods (except for streams) will return a Promise in the event
+ * that a callback is omitted.
+ */
+common.util.promisifyAll(InstanceTemplate);
+
+module.exports = InstanceTemplate;
---
+++ test/instance-template.js
@@ -0,0 +1,249 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var assert = require('assert');
+var extend = require('extend');
+var nodeutil = require('util');
+var proxyquire = require('proxyquire');
+var ServiceObject = require('@google-cloud/common').ServiceObject;
+var util = require('@google-cloud/common').util;
+
+var promisified = false;
+var fakeUtil = extend({}, util, {
+  promisifyAll: function(Class) {
+    if (Class.name === 'InstanceTemplate') {
+      promisified = true;
+    }
+  },
+});
+
+function FakeServiceObject() {
+  this.calledWith_ = arguments;
+  ServiceObject.apply(this, arguments);
+}
+
+nodeutil.inherits(FakeServiceObject, ServiceObject);
+
+describe('InstanceTemplate', function() {
+  var InstanceTemplate;
+  var instanceTemplate;
+
+  var INSTANCE_TEMPLATE_NAME = 'my-instance-template';
+  var COMPUTE = {};
+
+  before(function() {
+    InstanceTemplate = proxyquire('../src/instance-template.js', {
+      '@google-cloud/common': {
+        ServiceObject: FakeServiceObject,
+        util: fakeUtil,
+      },
+    });
+  });
+
+  beforeEach(function() {
+    instanceTemplate = new InstanceTemplate(COMPUTE, INSTANCE_TEMPLATE_NAME);
+  });
+
+  describe('instantiation', function() {
+    it('should localize the name', function() {
+      assert.strictEqual(instanceTemplate.name, INSTANCE_TEMPLATE_NAME);
+    });
+
+    it('should promisify all the things', function() {
+      assert(promisified);
+    });
+
+    it('should inherit from ServiceObject', function(done) {
+      var instanceTemplate = new InstanceTemplate(
+        COMPUTE,
+        INSTANCE_TEMPLATE_NAME
+      );
+      assert(instanceTemplate instanceof ServiceObject);
+
+      var calledWith = instanceTemplate.calledWith_[0];
+
+      assert.strictEqual(calledWith.parent, COMPUTE);
+      assert.strictEqual(calledWith.baseUrl, '/global/instanceTemplates');
+      assert.strictEqual(calledWith.id, INSTANCE_TEMPLATE_NAME);
+      assert.deepEqual(calledWith.methods, {
+        exists: true,
+        get: true,
+        getMetadata: true,
+      });
+      done();
+    });
+  });
+
+  describe('create', function() {
+    var OPTIONS = {};
+
+    it('should make the correct API request', function(done) {
+      instanceTemplate.request = function(reqOpts) {
+        assert.strictEqual(reqOpts.method, 'POST');
+        assert.strictEqual(reqOpts.uri, '');
+        assert.strictEqual(reqOpts.json.name, INSTANCE_TEMPLATE_NAME);
+        done();
+      };
+
+      instanceTemplate.create(assert.ifError);
+    });
+
+    describe('error', function() {
+      var error = new Error('Error.');
+      var apiResponse = {a: 'b', c: 'd'};
+
+      beforeEach(function() {
+        instanceTemplate.request = function(reqOpts, callback) {
+          callback(error, apiResponse);
+        };
+      });
+
+      it('should return an error if the request fails', function(done) {
+        instanceTemplate.create(OPTIONS, function(
+          err,
+          instanceTemplate,
+          operation,
+          apiResponse_
+        ) {
+          assert.strictEqual(err, error);
+          assert.strictEqual(instanceTemplate, null);
+          assert.strictEqual(operation, null);
+          assert.strictEqual(apiResponse_, apiResponse);
+          done();
+        });
+      });
+
+      it('should not require a callback', function() {
+        assert.doesNotThrow(function() {
+          instanceTemplate.create(OPTIONS);
+        });
+      });
+    });
+
+    describe('success', function() {
+      var apiResponse = {
+        name: 'op-name',
+      };
+
+      beforeEach(function() {
+        instanceTemplate.request = function(reqOpts, callback) {
+          callback(null, apiResponse);
+        };
+      });
+
+      it('should execute callback with Operation & Response', function(done) {
+        var operation = {};
+
+        instanceTemplate.operation = function(name) {
+          assert.strictEqual(name, apiResponse.name);
+          return operation;
+        };
+
+        instanceTemplate.create(OPTIONS, function(
+          err,
+          instanceTemplate,
+          operation_,
+          apiResponse_
+        ) {
+          assert.ifError(err);
+          assert.strictEqual(operation_, operation);
+          assert.strictEqual(apiResponse_, apiResponse);
+          done();
+        });
+      });
+
+      it('should not require a callback', function() {
+        assert.doesNotThrow(function() {
+          instanceTemplate.create(OPTIONS);
+        });
+      });
+    });
+  });
+
+  describe('delete', function() {
+    it('should call ServiceObject.delete', function(done) {
+      FakeServiceObject.prototype.delete = function() {
+        assert.strictEqual(this, instanceTemplate);
+        done();
+      };
+
+      instanceTemplate.delete();
+    });
+
+    describe('error', function() {
+      var error = new Error('Error.');
+      var apiResponse = {a: 'b', c: 'd'};
+
+      beforeEach(function() {
+        FakeServiceObject.prototype.delete = function(callback) {
+          callback(error, apiResponse);
+        };
+      });
+
+      it('should return an error if the request fails', function(done) {
+        instanceTemplate.delete(function(err, operation, apiResponse_) {
+          assert.strictEqual(err, error);
+          assert.strictEqual(operation, null);
+          assert.strictEqual(apiResponse_, apiResponse);
+          done();
+        });
+      });
+
+      it('should not require a callback', function() {
+        assert.doesNotThrow(function() {
+          instanceTemplate.delete();
+        });
+      });
+    });
+
+    describe('success', function() {
+      var apiResponse = {
+        name: 'op-name',
+      };
+
+      beforeEach(function() {
+        FakeServiceObject.prototype.delete = function(callback) {
+          callback(null, apiResponse);
+        };
+      });
+
+      it('should execute callback with Operation & Response', function(done) {
+        var operation = {};
+
+        instanceTemplate.operation = function(name) {
+          assert.strictEqual(name, apiResponse.name);
+          return operation;
+        };
+
+        instanceTemplate.delete(function(err, operation_, apiResponse_) {
+          assert.ifError(err);
+          assert.strictEqual(operation_, operation);
+          assert.strictEqual(operation_.metadata, apiResponse);
+          assert.strictEqual(apiResponse_, apiResponse);
+          done();
+        });
+      });
+
+      it('should not require a callback', function() {
+        assert.doesNotThrow(function() {
+          instanceTemplate.delete();
+        });
+      });
+    });
+  });
+});
