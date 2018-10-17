259609f45c76c34b62e91b7f16a1d71bd57a2a89
Stephen Sawchuk <stephenplusplus@gmail.com>
2018-10-17 14:14:55 -0400

    Update docs & let -> const

--- src/vm.js
+++ src/vm.js
@@ -725,9 +725,11 @@ VM.prototype.resize = function(machineType, options, callback) {
 };
 
 /**
- * Set the custom metadata for this instance. This uses `PATCH` semantics by
- * default, which means that values will be overwritten with the passed in
- * metadata, but keys that were not overwritten will also exist.
+ * Set the custom metadata for this instance.
+ *
+ * This will combine the `metadata` key/value pairs with any pre-existing
+ * metadata. Any changes will override pre-existing keys. To remove a
+ * pre-existing key, explicitly set the key's value to `null`.
  *
  * @see [Instances: setMetadata API Documentation]{@link https://cloud.google.com/compute/docs/reference/v1/instances/setMetadata}
  *
@@ -745,7 +747,8 @@ VM.prototype.resize = function(machineType, options, callback) {
  * const vm = zone.vm('vm-name');
  *
  * const metadata = {
- *   'startup-script': '...'
+ *   'startup-script': '...',
+ *   customKey: null // Setting `null` will remove the `customKey` property.
  * };
  *
  * vm.setMetadata(metadata, function(err, operation, apiResponse) {
@@ -786,7 +789,7 @@ VM.prototype.setMetadata = function(metadata, callback) {
 
     for (const key in newMetadataJSON) {
       if (newMetadataJSON.hasOwnProperty(key)) {
-        let value = newMetadataJSON[key];
+        const value = newMetadataJSON[key];
 
         if (value !== null) {
           request.items.push({key, value});
