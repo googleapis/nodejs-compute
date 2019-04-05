f32dd9c4e061a89f26d84c7f1c5809a6021730d9
Jonathan Lui <jonathanlui@google.com>
2019-04-04 18:27:27 -0700

    lint

--- samples/test/samples.test.js
+++ samples/test/samples.test.js
@@ -20,7 +20,7 @@ const cp = require('child_process');
 const {assert} = require('chai');
 const Compute = require('@google-cloud/compute');
 
-const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 const compute = new Compute();
 
