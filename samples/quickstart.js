7b9da474c590a0eedd025064247762dc7baf7cf8
Alexander Fenster <fenster@google.com>
2021-01-05 14:27:32 -0800

    feat!: DIREGAPIC Compute alpha

--- samples/quickstart.js
+++ samples/quickstart.js
@@ -1,47 +1,52 @@
-// Copyright 2017, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+const compute = require('@google-cloud/compute');
 
-'use strict';
-
-async function main(
-  vmName = 'new_virtual_machine' // VM name of your choice
-) {
-  // [START compute_engine_quickstart]
-  // Imports the Google Cloud client library
-  const Compute = require('@google-cloud/compute');
-
-  // Creates a client
-  const compute = new Compute();
-
-  async function quickstart() {
-    // Create a new VM using the latest OS image of your choice.
-    const zone = compute.zone('us-central1-c');
-
-    // TODO(developer): choose a name for the VM
-    // const vmName = 'vm-name';
-
-    // Start the VM create task
-    const [vm, operation] = await zone.createVM(vmName, {os: 'ubuntu'});
-    console.log(vm);
-
-    // `operation` lets you check the status of long-running tasks.
-    await operation.promise();
-
-    // Complete!
-    console.log('Virtual machine created!');
-  }
-  quickstart();
-  // [END compute_engine_quickstart]
+function sleep(timeoutMs) {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeoutMs);
+  });
 }
 
-main(...process.argv.slice(2));
+// point GOOGLE_APPLICATION_CREDENTIALS environment variable to
+// the JSON service account key file
+
+async function main() {
+  const client = new compute.AddressesClient({fallback: 'rest'});
+  const project = 'PROJECT-NAME';
+  const region = 'us-east1';
+
+  const addressResource = {
+    name: 'test-address-123',
+  };
+
+  const [insertResponse] = await client.insert({
+    project,
+    region,
+    addressResource,
+  });
+  console.log(insertResponse);
+
+  await sleep(5000);
+
+  const [listResponse1] = await client.list({
+    project,
+    region,
+  });
+  console.log(listResponse1);
+
+  const [deleteResponse] = await client.delete({
+    project,
+    region,
+    address: addressResource.name,
+  });
+  console.log(deleteResponse);
+
+  await sleep(5000);
+
+  const [listResponse2] = await client.list({
+    project,
+    region,
+  });
+  console.log(listResponse2);
+}
+
+main();
