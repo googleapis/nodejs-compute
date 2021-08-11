c5bf3f57118b31279f7d1eb1ad7ec932c1ee42f6
Fedor Isakov <isakovf@google.com>
2021-08-11 11:51:30 +0300

    lint fixes

--- samples/createInstance.js
+++ samples/createInstance.js
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const { operationsProtos } = require('google-gax');
-
 /**
  * Sends an instance creation request to GCP and waits for it to complete.
  *
