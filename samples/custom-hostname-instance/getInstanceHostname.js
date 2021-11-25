3378b6a5ead57a88a067cb91cc41713b101b8b8f
Fedor Isakov <isakovf@google.com>
2021-11-25 23:51:50 +0300

    chore(samples): add custom hostname instances samples

---
+++ samples/custom-hostname-instance/getInstanceHostname.js
@@ -0,0 +1,49 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * Prints a hostname of the GCP VM instance.
+ *
+ * @param {string} projectId - ID or number of the project you want to list images from
+ * @param {string} zone - name of the zone your instance belongs to, for example: us-west3-b
+ * @param {string} instanceName - Name of the new machine.
+ */
+function main(projectId, zone, instanceName) {
+  // [START compute_instances_get_hostname]
+  /**
+   * TODO(developer): Uncomment and replace these variables before running the sample.
+   */
+  // const projectId = 'YOUR_PROJECT_ID';
+  // const zone = 'europe-central2-b'
+  // const instanceName = 'YOUR_INSTANCE_NAME'
+
+  const compute = require('@google-cloud/compute');
+
+  async function getInstanceHostname() {
+    const instancesClient = new compute.InstancesClient();
+
+    const [instance] = await instancesClient.get({
+      project: projectId,
+      zone,
+      instance: instanceName,
+    });
+    // If a custom hostname is not set, the output for instance.hostname will be undefined
+    console.log(`Instance ${instanceName} has hostname: ${instance.hostname}`);
+  }
+
+  getInstanceHostname();
+  // [END compute_instances_get_hostname]
+}
+
+main(...process.argv.slice(2));
