b6330a4ba470a7cc07f772e239465721c2be58b6
sofisl <55454395+sofisl@users.noreply.github.com>
2022-11-17 17:41:26 -0800

    build: update README for deprecation notice and delete all files except samples (#869)

--- owlbot.py
+++ owlbot.py
@@ -1,6 +1,6 @@
-# Copyright 2021 Google LLC
+# Copyright 2022 Google LLC
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the License);
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -11,10 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import synthtool.languages.node as node
-
 node.owlbot_main(templates_excludes=[
-  '.mocharc.js',
-  '.github/workflows/ci.yaml',
+'README.md'
 ])
